010fb5b26f02e963d37eb764cce6df768e199125
Laurence Leveque <laureleveque@free.fr>
2022-04-05 19:13:28 +0200

    ajout image page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,53 +1,48 @@
-
 // récupération du paramètre id de l'URL
 
-const url = new URL( window.location.href );
+const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
 console.log(id);
 
-
 // Envoi de la requête spécifique à chaque id
 
-fetch("http://localhost:3000/api/products/" + id) 
- .then(function (res) { 
-    if (res.ok) { 
- 
-        return res.json(); 
- }
- })
- .then(function (value) { 
- console.log(value); 
+fetch("http://localhost:3000/api/products/" + id)
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (canape) {
+    console.log(canape);
 
+    // création de l'élément image
+    const image = document.createElement("img");
+    image.src = canape.imageUrl; // insère imageUrl dans l'attribut src de la balise image
+    image.alt = canape.altTxt; // modifie le contenu de alt
 
-// récupération de l'image
-const photo= document.getElementsByClassName("item__img");
+    //récupération de la div de l'image
+    const divImage = document.getElementsByClassName("item__img")[0];
 
+    // insertion de l'image
+    divImage.appendChild(image);
 
-// récupération de l'élément titre   
-const titre= document.getElementById("title");
-titre.innerHTML= value.name;
+    // récupération de l'élément titre
+    const titre = document.getElementById("title");
+    titre.innerHTML = canape.name;
 
+    // récupération de l'élément prix
+    const prix = document.getElementById("price");
+    prix.innerHTML = canape.price;
 
-// récupération de l'élément prix 
-const prix=document.getElementById("price");
-prix.innerHTML= value.price;
+    // récupération de l'élément description
+    const paragraphe = document.getElementById("description");
+    paragraphe.innerHTML = canape.description;
 
+    // récupération de l'élément de sélection de couleur
+    const couleurs = document.getElementById("colors");
 
-// récupération de l'élément description 
-const paragraphe=document.getElementById("description");
-paragraphe.innerHTML=value.description;
-
-
-// choix de la couleur
-
-
-
-// choix de la quantité
-
-
-
-//  si code statut erroné
-}) 
-.catch(function (err) {
-  console.error(err);
-}); 
+    //  si code statut erroné
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
--- front/js/script.js
+++ front/js/script.js
@@ -1,62 +1,67 @@
 // envoi requête Get - Récupération des données de l'API
 
-fetch("http://localhost:3000/api/products") 
-  .then(function (res) {  // réponse à la requête
-    if (res.ok) { // vérification déroulement de la requête
-      
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+    // réponse à la requête
+    if (res.ok) {
+      // vérification déroulement de la requête
+
       return res.json(); // résultat de la requête au format JSON (Promise)
     }
   })
-  .then(function (value) { // récupération JSON dans value ( tableau des produits)
-    console.log(value); 
-
+  .then(function (tousLesCanapes) {
+    // récupération JSON dans value ( tableau des produits)
+    console.log(tousLesCanapes);
 
     //récupération des éléments du tableau et insertion de chaque élément dans la page d'accueil
 
-
     const section = document.getElementById("items"); // recherche dans le document l'élément dont l'id est "items (récupère la balise où afficher les produits)
 
-    for (let i = 0; i < value.length; i++) { // itération sur tous les produits du tableau
+    /*for (const canape of tousLesCanapes) {
+      
+    }
 
-      const element = value[i]; // element = un produit [index dans le tableau]
+    tousLesCanapes.forEach(canape => {
+      
+    });*/
 
+    for (let i = 0; i < tousLesCanapes.length; i++) {
+      // itération sur tous les produits du tableau
 
-      // création balises à l'intérieur de la balise section
+      const canape = tousLesCanapes[i]; // canapé = un produit [index dans le tableau]
 
-      const lien = document.createElement("a"); 
-      const article = document.createElement("article"); 
-      const image = document.createElement("img"); 
+      // création des balises à l'intérieur de la balise section et stockage dans les const
+
+      const lien = document.createElement("a");
+      const article = document.createElement("article");
+      const image = document.createElement("img");
       const titre = document.createElement("h3");
       const paragraphe = document.createElement("p");
 
-
       // modification des contenus
 
-      lien.href = "./product.html?id=" + element._id; // modification de l'attribut href de la balise a
+      lien.href = "./product.html?id=" + canape._id; // modification de l'attribut href de la balise a
 
-      image.src=element.imageUrl; // insère imageUrl dans l'attribut src de la balise image
-      image.alt=element.altTxt; // modifie le contenu de alt
+      image.src = canape.imageUrl; // insère imageUrl dans l'attribut src de la balise image
+      image.alt = canape.altTxt; // modifie le contenu de alt
 
-      titre.innerHTML=element.name; // modification titre à l'intérieur de la balise h3
+      //image.setAttribute("src", canape.imageUrl);
 
-      paragraphe.innerHTML=element.description; // modification texte à l'intérieur de la balise p
+      titre.innerHTML = canape.name; // modification titre à l'intérieur de la balise h3
 
+      paragraphe.innerHTML = canape.description; // modification texte à l'intérieur de la balise p
 
       // ajout des éléments en tant qu'enfant (pour les voir sur la page)
 
-      lien.appendChild(article); 
+      lien.appendChild(article);
       section.appendChild(lien);
-      article.appendChild(image);     
+      article.appendChild(image);
       article.appendChild(titre);
       article.appendChild(paragraphe);
     }
 
     //  si code statut erroné
-  }) 
+  })
   .catch(function (err) {
     console.error(err);
-  }); 
-
-  
-
-
+  });
